53b9936c6ca0562e0f5a5dc55014f79835dd96cc
Yannick Voyer <yan.voyer@gmail.com>
2013-03-28 22:59:11 -0400

    Add Type tests

--- src/Star/Component/Hoard/Equipment/Type/Type.php
+++ src/Star/Component/Hoard/Equipment/Type/Type.php
@@ -6,39 +6,129 @@
 
 namespace Star\Component\Hoard\Equipment\Type;
 
-abstract class Type
+use Star\Component\Hoard\Equipment\Exception\AttributeNotNullableException;
+
+/**
+ * @author Yannick Voyer
+ *
+ * Class defining the types of equipments
+ */
+class Type
 {
-    private $types = array();
+    const IDENTIFIER_WEAPON = "WEAPON";
+    const IDENTIFIER_MAGIC = "MAGIC";
+    const IDENTIFIER_MASTERWORK = "MASTERWORK";
 
     /**
-     * Returns the cost
+     * The name of the type
      *
-     * @return float
+     * @var string
      */
-    public abstract function getCost();
+    private $name;
 
     /**
-     * Returns the cost of the equipment
+     * The identifiers
      *
-     * @return float
+     * @var array of identifiers
      */
-    protected function addType($type)
+    private $identifiers = array();
+
+    public function __construct($name = null, array $identifiers = array())
     {
-        $this->types[] = $type;
+        $this->name = $name;
+        foreach ($identifiers as $identifier) {
+            $this->addIdentifier($identifier);
+        }
     }
 
+    /**
+     * Returns the string representation of the type
+     *
+     * @return string
+     */
     public function __toString()
     {
-        return implode(",", $this->types);
+        return $this->getName();
     }
 
     /**
-     * Returns whether the type is considered magic
+     * Returns the exported version for printing in a file
      *
-     * @return boolean
-    */
+     * @return string
+     */
+    public function export()
+    {
+        // @todo Move to a decorator
+        return "Type:" . implode(",", $this->identifiers);
+    }
+
+    /**
+     * Returns the name of the type
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        if (empty($this->name)) {
+            throw new AttributeNotNullableException("name");
+        }
+
+        return $this->name;
+    }
+
+    /**
+     * Add an identifier on the type
+     *
+     * @param string $identifier
+     */
+    private function addIdentifier($identifier)
+    {
+        $this->identifiers[$identifier] = $identifier;
+    }
+
+    /**
+     * Returns the identifiers
+     *
+     * @return array
+     */
+    public function getIdentifiers()
+    {
+        if (empty($this->identifiers)) {
+            throw new AttributeNotNullableException("identifiers");
+        }
+
+        return array_values($this->identifiers);
+    }
+
+    /**
+     * Returns whether the $identifier is present in the collection
+     *
+     * @param string $identifier
+     *
+     * @return bool
+     */
+    private function hasIdentifier($identifier)
+    {
+        return array_key_exists($identifier, $this->identifiers);
+    }
+
+    /**
+     * Retruns whether the type is magical
+     *
+     * @return bool
+     */
     public function isMagic()
     {
+        return $this->hasIdentifier(self::IDENTIFIER_MAGIC);
+    }
 
+    /**
+     * Retruns whether the type is masterwork
+     *
+     * @return bool
+     */
+    public function isMasterwork()
+    {
+        return $this->hasIdentifier(self::IDENTIFIER_MASTERWORK) || $this->isMagic();
     }
 }
---
+++ src/Star/Component/Hoard/Tests/Equipment/Type/TypeTest.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * @author  Yannick Voyer (http://github.com/yvoyer)
+ * @package Hoard Component
+ */
+
+namespace Star\Component\Hoard\Equipment\Type;
+
+use Star\Component\Hoard\Tests\HoardTestCase;
+use Star\Component\Hoard\Equipment\Type\Type;
+
+class TypeTest extends HoardTestCase
+{
+    public function getType($name = null, array $identifiers = array())
+    {
+        return new Type($name, $identifiers);
+    }
+
+    /**
+     * @expectedException        Star\Component\Hoard\Equipment\Exception\AttributeNotNullableException
+     * @expectedExceptionMessage The attribute 'name' can't be null.
+     */
+    public function testNameCantBeNull()
+    {
+        $this->getType()->getName();
+    }
+
+    public function testGetNameReturnsTheSetValue()
+    {
+        $expectedValue = uniqid();
+
+        $type = $this->getType($expectedValue);
+        $this->assertSame($expectedValue, $type->getName());
+    }
+
+    /**
+     * @expectedException        Star\Component\Hoard\Equipment\Exception\AttributeNotNullableException
+     * @expectedExceptionMessage The attribute 'identifiers' can't be null.
+     */
+    public function testIdentifiersCantBeNullable()
+    {
+        $this->getType()->getIdentifiers();
+    }
+
+    public function testGetIdentifierReturnsTheInternalsValues()
+    {
+        $identifiers = array(
+            uniqid(),
+        );
+
+        $type = $this->getType(null, $identifiers);
+        $this->assertSame($identifiers, $type->getIdentifiers());
+    }
+
+    public function testToStringReturnsTheName()
+    {
+        $name = uniqid();
+        $type = $this->getType($name);
+
+        $this->assertSame($name, $type->__toString());
+    }
+
+    public function testExportReturnsTheTypeForPrintingInAFile()
+    {
+        $name = uniqid();
+        $identifiers = array(
+            Type::IDENTIFIER_WEAPON,
+            Type::IDENTIFIER_MASTERWORK,
+        );
+        $type = $this->getType($name, $identifiers);
+
+        $expects = "Type:" . implode(",", $identifiers);
+        $this->assertSame($expects, $type->export());
+    }
+
+    public function testIsMagic()
+    {
+        $identifiers = array(
+            Type::IDENTIFIER_WEAPON,
+        );
+        $type = $this->getType(null, $identifiers);
+        $this->assertFalse($type->isMagic(), "The default value should be non magical");
+
+        $identifiers = array(
+            Type::IDENTIFIER_MAGIC,
+        );
+        $type = $this->getType(null, $identifiers);
+        $this->assertTrue($type->isMagic(), "The equipment should be magical");
+    }
+
+    public function testIsMasterwork()
+    {
+        $identifiers = array(
+            Type::IDENTIFIER_WEAPON,
+        );
+        $type = $this->getType(null, $identifiers);
+        $this->assertFalse($type->isMasterwork(), "The default value should be non masterwork");
+
+        $identifiers = array(
+            Type::IDENTIFIER_MASTERWORK,
+        );
+        $type = $this->getType(null, $identifiers);
+        $this->assertTrue($type->isMasterwork(), "The equipment should be masterwork");
+
+        $identifiers = array(
+            Type::IDENTIFIER_MAGIC,
+        );
+        $type = $this->getType(null, $identifiers);
+        $this->assertTrue($type->isMasterwork(), "The equipment should be masterwork when magic");
+    }
+}
